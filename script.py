906e46a9542c92a218ec2a632d99701345ac90bd
duner <asduner@gmail.com>
2016-04-26 20:11:36 -0500

    it works

--- script.py
+++ script.py
@@ -1,18 +1,35 @@
+import os
 import urllib
 import requests
 import tweepy
 from bs4 import BeautifulSoup
 
+REQUEST_HEADER = {
+    'User-Agent': 'WikipediaBits Twitter Bot',
+    'From': os.environ.get('EMAIL_ADDRESS')
+}
+
 BASE_URL = 'http://en.wikipedia.org/w/api.php?format=json&action=query&'
 EXCEPTIONS = ['list','refer','these','things']
 
-def get_random_page():
-	rand_url = BASE_URL + '&list=random&rnnamespace=0'
-	rand_response = requests.get(rand_url).json()
-	rand_id = rand_response['query']['random'][0]['id']
-	get_article(rand_id)
+auth = tweepy.OAuthHandler(
+	os.environ.get('TWITTER_CONSUMER_KEY'),
+	os.environ.get('TWITTER_CONSUMER_SECRET')
+)
+auth.set_access_token(
+	os.environ.get('TWITTER_ACCESS_TOKEN'),
+	os.environ.get('TWITTER_ACCESS_TOKEN_SECRET')
+)
+api = tweepy.API(auth)
 
-def get_article(id):
+
+def get_random_page_id():
+	rand_url = BASE_URL + '&list=random&rnnamespace=0'
+	rand_response = requests.get(rand_url, headers=REQUEST_HEADER).json()
+	rand_id = rand_response['query']['random'][0]['id']
+	return rand_id
+
+def get_page_content_if_under_char_limit(id):	
 	params = {
 		'prop': 'extracts',
 		'redirects': 'true',
@@ -20,19 +37,17 @@ def get_article(id):
 		'pageids': str(id),
 	}
 	page_url = BASE_URL + urllib.urlencode(params)
-	page = requests.get(page_url).json()
+	page = requests.get(page_url, headers=REQUEST_HEADER).json()
 	extract = page['query']['pages'][str(id)]['extract']
 	text = get_page_text(extract)
 	if fits_in_tweet(text):
 		content = text.split('\n')[0].strip('\n')
-		post_tweet(content)
+		return content.encode('utf8')
 	else:
-		get_random_page()
+		return False
 
 def get_page_text(extract):
-	soup = BeautifulSoup(extract)
-	text = soup.get_text()
-	return text
+	return BeautifulSoup(extract).get_text()
 
 def fits_in_tweet(text):
 	chars = len(text)
@@ -44,10 +59,17 @@ def fits_in_tweet(text):
 		else:
 			return True
 
-def post_tweet(tweet_content):
-	print tweet_content
+def post_tweet(content):
+	api.update_status(status=content)
+	print("TWEETED: " + content)
 
+def main():
+	found_page = False
+	while not found_page:
+		page_id = get_random_page_id()
+		content = get_page_content_if_under_char_limit(page_id)
+		if content:
+			found_page = True
+			post_tweet(content)
 
-
-
-get_random_page()
+main()
