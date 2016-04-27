cc8d91695557516bb1f46debd94e179d71a2bb1a
duner <asduner@gmail.com>
2016-04-26 22:18:17 -0500

    redis

--- script.py
+++ script.py
@@ -1,16 +1,19 @@
 import os
-import urllib
+import random
+import redis
 import requests
 import tweepy
+import urllib
+
 from bs4 import BeautifulSoup
 
 REQUEST_HEADER = {
     'User-Agent': 'WikipediaBits Twitter Bot',
     'From': os.environ.get('EMAIL_ADDRESS')
 }
-
 BASE_URL = 'http://en.wikipedia.org/w/api.php?format=json&action=query&'
 EXCEPTIONS = ['list','refer','these','things']
+ODDS = 3 if os.environ.get("ON_HEROKU", False) else 1
 
 auth = tweepy.OAuthHandler(
 	os.environ.get('TWITTER_CONSUMER_KEY'),
@@ -22,6 +25,7 @@ auth.set_access_token(
 )
 api = tweepy.API(auth)
 
+r = redis.from_url(os.environ.get("REDIS_URL"))
 
 def get_random_page_id():
 	rand_url = BASE_URL + '&list=random&rnnamespace=0'
@@ -37,14 +41,15 @@ def get_page_content_if_under_char_limit(id):
 		'pageids': str(id),
 	}
 	page_url = BASE_URL + urllib.urlencode(params)
-	page = requests.get(page_url, headers=REQUEST_HEADER).json()
-	extract = page['query']['pages'][str(id)]['extract']
-	text = get_page_text(extract)
+	page_response = requests.get(page_url, headers=REQUEST_HEADER).json()
+	page_data = page_response['query']['pages'][str(id)]
+	text = get_page_text(page_data['extract'])
+	print("Checking: '{}'".format(page_data['title'].encode('utf-8')))
 	if fits_in_tweet(text):
 		content = text.split('\n')[0].strip('\n')
-		return content.encode('utf8')
+		return (content.encode('utf8'), page_data)
 	else:
-		return False
+		return False, page_data
 
 def get_page_text(extract):
 	return BeautifulSoup(extract).get_text()
@@ -60,17 +65,33 @@ def fits_in_tweet(text):
 			return True
 
 def post_tweet(content):
-	api.update_status(status=content)
+	status = api.update_status(status=content)
 	print("TWEETED: " + content)
+	return status
+
+def save_to_redis(content, page_data, status):
+	data = {
+	    'title': page_data['title'].encode('utf-8'),
+	    'wikipedia_id': page_data['pageid'],
+	    'content': content,
+	    'twitter_id': status.id,
+	    'time_tweeted': status.created_at.strftime("%Y-%m-%d %H:%M:%S")
+	}
+	r.hmset(wikipedia_id, data)
+	print("SAVED TO REDIS")
 
 def main():
 	found_page = False
 	while not found_page:
 		page_id = get_random_page_id()
-		content = get_page_content_if_under_char_limit(page_id)
+		content, page_data = get_page_content_if_under_char_limit(page_id)
 		if content:
 			found_page = True
-			post_tweet(content)
+			status = post_tweet(content)
+			save_to_redis(content, page_data, status)
 
 if __name__ == "__main__":
-	main()
+	if random.choice(range(ODDS)) == 0:
+		main()
+	else:
+		print("SKIPPING")
